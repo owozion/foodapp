bf0bd7eedbb303839903a79220b96ec3eb3de161
owozion <j4owolabi@gmail.com>
2024-05-21 14:52:13 +0100

    first commit

---
+++ src/restaurants/Quote.js
@@ -0,0 +1,21 @@
+export const Quote = ({quotes, calfunc}) => {
+
+calfunc(20)
+    return(
+        <div>
+            <h1>Quotes</h1>
+
+            <ul className="list-group">
+                {
+                    quotes.map((quo, index)=>{
+                        return(
+                            <li className="list-group-item" key={index}>{quo.text}<span>{quo.author}</span></li>  
+                        )
+                    })
+                }
+                
+            </ul>
+            <button onClick={()=>{calfunc(8)}}>Click Me</button>
+        </div>
+    )
+}
